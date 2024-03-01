09b279f5c58bbcdddc42a18c84679567202d62a4
DhruvikPrj <dhruvik.prajapati@bypeopletechnologies.com>
2024-03-01 17:15:02 +0530

    fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:riverpod_practice/Theme_Toggle/material-theme/main.g.dart';
+import 'package:riverpod_practice/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
